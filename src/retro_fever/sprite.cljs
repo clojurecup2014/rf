ed1e81136e757cd46c50264ffa9df249c30d47c5
Kalina Todorova <kt@natur-energi.dk>
2014-09-27 14:17:16 +0200

    Updated sprite structure with size

--- src/retro_fever/sprite.cljs
+++ src/retro_fever/sprite.cljs
@@ -1,35 +1,40 @@
 (ns retro-fever.sprite)
 
-(def sprite-state (atom {:image ""
-                         :canvas ""}))
+(def sprite-state (atom {}))
 
 
-(defn  ^:export init-image
+(defn  ^:export init-sprite
   ""
-  [src]
+  [src width height x y]
   (let [image (js/Image.)]
     (set! (.-src image) src)
-    image))
+    {:image image
+     :width width
+     :height height
+     :x x
+     :y y}))
 
 (defn init-canvas [id width height]
   (let [canvas (.getElementById js/document (name id))]
     (set! (.-width canvas) width)
     (set! (.-height canvas) height)
-    (.getContext canvas "2d")))
+    {:canvas-2d (.getContext canvas "2d")
+     :canvas-width width
+     :canvas-height height}))
 
-(defn ^:export render [canvas image]
-  (let [width (.-width image)]
-    (.log js/console width)
+(defn ^:export render [canvas sprite]
+  (let  [{:keys [image width height x y]} sprite
+         {:keys [canvas-2d canvas-width canvas-height]} canvas]
     (.log js/console canvas)
-    (set! (.-fillStyle canvas) "black")
-    (.drawImage canvas image 0 0 100 100 0 0 10 10)))
+    (.log js/console canvas-width)
+    (.drawImage canvas-2d image 0 0 canvas-width canvas-height x y width height)))
 
 (defn game-loop []
-  (render (:canvas @sprite-state) (:image @sprite-state)))
+  (render (:canvas @sprite-state) (:sprite @sprite-state)))
 
 (defn  ^:export init []
-  (let [image (init-image "images/coin-sprite-animation.png" 10)
+  (let [sprite (init-sprite "images/coin-sprite-animation.png" 10 10 0 0)
         canvas (init-canvas "coinAnimation" 100 100)]
-    (swap! sprite-state update-in [:image] (fn [] image))
+    (swap! sprite-state update-in [:sprite] (fn [] sprite))
     (swap! sprite-state update-in [:canvas]  (fn [] canvas))
-    (.addEventListener image "load" game-loop)))
+    (.addEventListener (:image sprite) "load" game-loop)))
