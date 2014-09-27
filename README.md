9ef83bd2f9d4807665dd264ad00a3d4f7539bdba
Jacob Emcken <je@casalogic.dk>
2014-09-27 02:16:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Retro Fever
+
+A 2D game engine targeting modern browsers and mobile devices
+
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject retro-fever "0.1.0-SNAPSHOT"
+  :description "A 2D game engine targeting modern browsers and mobile devices"
+  :url "http://rf.clojurecup.com"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2356"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src"]
+        :compiler {
+          :output-to "resources/retro-fever.js"
+          :optimizations :whitespace
+          :pretty-print true}}]}
+)
