749b442073117cf56e7a657191d0a792caf739f8
Jacob Emcken <je@casalogic.dk>
2014-09-27 21:26:53 +0200

    Move example1 to a self contained demo

---
+++ demo/rf-basic-game/project.clj
@@ -0,0 +1,14 @@
+(defproject rf-basic-game "0.1.0-SNAPSHOT"
+  :description "Demoing a basic game using Retro Fever"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2356"]
+                 [retro-fever "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src"]
+        :compiler {
+          :output-to "resources/rf-basic-game.js"
+          :optimizations :whitespace
+          :pretty-print true}}]}
+)
