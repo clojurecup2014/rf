c0483a70c96c09f517e05ad5d5d56881ddb84d7c
Kalina Todorova <kt@natur-energi.dk>
2014-09-27 07:39:22 +0200

    Code style fixed

--- src/retro_fever/sprite.cljs
+++ src/retro_fever/sprite.cljs
@@ -1,13 +1,13 @@
 (ns retro-fever.sprite)
 
 (defn  ^:export init-image
-""
-[src width]
-(let [image (js/Image.)]
-  (set! (.-onload image) (fn []
-                           (set! (.-width image) width)))
-  (set! (.-src image) src)
-  image))
+  ""
+  [src width]
+  (let [image (js/Image.)]
+    (set! (.-onload image) (fn []
+                             (set! (.-width image) width)))
+    (set! (.-src image) src)
+    image))
 
 (defn init-canvas [id width height]
   (let [canvas (.getElementById js/document (name id))]
@@ -15,11 +15,11 @@
     (set! (.-height canvas) height)
     (.getContext canvas "2d")))
 
- (defn ^:export render [canvas image]
-   (set! (.-onload image) (fn []
-                          (do (.log js/console canvas)
-                              (set! (.-fillStyle canvas) "black")
-                              (.drawImage canvas image 0 0 100 100 0 0 100 100)))))
+(defn ^:export render [canvas image]
+  (set! (.-onload image) (fn []
+                           (do (.log js/console canvas)
+                               (set! (.-fillStyle canvas) "black")
+                               (.drawImage canvas image 0 0 100 100 0 0 100 100)))))
 
 
 (defn  ^:export init []
