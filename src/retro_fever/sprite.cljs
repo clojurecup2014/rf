285c1ec90e9bda4a1f5c11c1d02513377a7e1db0
Claus Engel-Christensen <cec@casalogic.dk>
2014-09-28 17:49:53 +0200

    Added new render functions used to render images on a HTML canvas

--- src/retro_fever/sprite.cljs
+++ src/retro_fever/sprite.cljs
@@ -8,24 +8,14 @@
     image))
 
 (defn ^:export sprite
-  [image width height x y]
-  {:image image
-   :width width
-   :height height
-   :x x
-   :y y})
-
-(defn ^:export render [context sprite]
-  (let [{:keys [image width height x y]} sprite]
-    (.save context)
-    (.translate context x y)
-    (.drawImage context image (* (/ width 2) -1) (* (/ height 2) -1) width height)
-    (.restore context)))
-
-(defn ^:export draw-image
-  "Draw given image with top left at specified location"
-  [context image x y]
-  (.drawImage context image x y (aget image "width") (aget image "height")))
+  "Creates a sprite record based on graphics resource"
+  ([image x y]
+     (sprite image (:width image) (:height image) x y))
+  ([image width height x y & [cell]]
+     (condp = (get-type image)
+       :spritesheet (SpritesheetSprite. image width height cell x y)
+       :animation (AnimatedSprite. image width height x y)
+       (ImageSprite. image width height x y))))
 
 (defn collides?
   [s1 s2]
@@ -38,11 +28,3 @@
         diff-y (- (:y s1) (:y s2))]
     (Math/sqrt (+ (* diff-x diff-x)
                   (* diff-y diff-y)))))
-
-(defn ^:export update [sprite]
-  (let [{:keys [x y velocity-x velocity-y acceleration]} sprite
-        new-vy (+ velocity-y 0.3)
-        new-vx (+ velocity-x 0.3)
-        new-x (+ x velocity-x)
-        new-y (+ y velocity-y)]
-    (assoc sprite :x new-x :y new-y :velocity-y new-vy :velocity-x new-vx)))
